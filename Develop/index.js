6fe0295b03ff4ecd4e11d3d8aec15e9260022483
Erick Michaelson <emich09@gmail.com>
2021-03-29 12:48:28 -0400

    added questions prompt using inquirer and began building the constant for the generated README template

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,61 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+let appLicenses = ['Academic Free License v3.0', 'Apache License v2.0', 'GNU General Public License v3.0', 'MIT License', 'Unlicense', 'N/A']
 
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            message: 'Please enter the title of your application: ',
+            name: 'appTitle'
+        },
+        {
+            type: 'input',
+            message: 'Enter a description of your application: ',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions: ',
+            name: 'install'
+        },  {
+            type: 'input',
+            message: 'Enter usage information: ',
+            name: 'usage'
+        },  {
+            type: 'input',
+            message: 'Enter contributor names, separated by commas: ',
+            name: 'contributors'
+        }, {
+            type: 'input',
+            message: 'Enter application testing processes: ',
+            name: 'test'
+        }, {
+            type: 'list',
+            message: 'Please select licenses used in your application',
+            list: appLicenses,
+            name: 'license'
+        }, {
+            type: 'input',
+            message: 'Enter Github username: ',
+            name: 'git'
+        }, {
+            type: 'input',
+            message: 'Enter email address:',
+            name: 'email'
+        }
+    ])
 
-];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+.then(function (responses) {
+    const readMe = `
     
-}
+    `
+});
 
 // TODO: Create a function to initialize app
 function init() {}
